a0d48887c7d1d7b74031ffb62be540ca11b1f365
Slava <slava.melanko@gmail.com>
2023-10-18 22:40:39 +0300

    Created HamburgerIcon

--- ui/src/component/mod.rs
+++ ui/src/component/mod.rs
@@ -1,7 +1,9 @@
+mod hamburger_icon;
 mod sidebar;
 mod theme_toggle;
 mod title;
 
+pub use crate::component::hamburger_icon::HamburgerIcon;
 pub use crate::component::sidebar::Sidebar;
 pub use crate::component::theme_toggle::ThemeToggle;
 pub use crate::component::title::Title;
--- ui/src/section/header.rs
+++ ui/src/section/header.rs
@@ -1,38 +1,9 @@
 use dioxus::prelude::*;
 
+use crate::component::HamburgerIcon;
 use crate::component::ThemeToggle;
 use crate::component::Title;
 use crate::theme::*;
-
-const NAV_ICON_JS_SCRIPT: &str = r#"
-    const toggleNavIcon = () => {
-        const navIcon = document.getElementById("nav-icon");
-        navIcon.classList.toggle("open");
-    };
-
-    const toggleSidebar = () => {
-        const modal = document.getElementById("sidebar-menu");
-        modal.style.display = modal.style.display === "block" ? "none" : "block";
-    };
-
-    document
-        .getElementById("nav-icon")
-        .addEventListener("click", function () {
-            toggleNavIcon();
-            toggleSidebar();
-        });
-
-    window.addEventListener("click", function (event) {
-        const targetId = event.target.id;
-        const modalOverlay = targetId === "sidebar-menu";
-        const anyMenuItem = targetId.includes("-item");
-
-        if (modalOverlay || anyMenuItem) {
-            toggleSidebar();
-            toggleNavIcon();
-        }
-    });
-    "#;
 
 #[derive(Props)]
 pub struct HeaderProps<'a> {
@@ -55,22 +26,7 @@ pub fn Header<'a>(cx: Scope<'a, HeaderProps<'a>>) -> Element {
 
             ThemeToggle {}
 
-            div {
-                id: "nav-icon",
-
-                span {
-                    style: "background-color: {theme.font.dark};",
-                }
-                span {
-                    style: "background-color: {theme.font.dark};",
-                }
-                span {
-                    style: "background-color: {theme.font.dark};",
-                }
-            }
-        }
-        script {
-            "{NAV_ICON_JS_SCRIPT}"
+            HamburgerIcon {}
         }
     })
 }
