75ffd5f56b756d44a858c131636119cf98aed352
Slava <slava.melanko@gmail.com>
2023-10-18 22:26:16 +0300

    Added the ThemeToggle component

--- ui/src/component/mod.rs
+++ ui/src/component/mod.rs
@@ -1,5 +1,7 @@
 mod sidebar;
+mod theme_toggle;
 mod title;
 
 pub use crate::component::sidebar::Sidebar;
+pub use crate::component::theme_toggle::ThemeToggle;
 pub use crate::component::title::Title;
